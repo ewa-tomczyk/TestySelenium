72defa619afb4ad13399b451b4937f49ccd9e67e
emevka <emevka@yahoo.com>
2021-11-10 14:16:23 +0100

    Adding to cart

--- src/test/java/Zadania/AutomationPracticeAddProductToCart.java
+++ src/test/java/Zadania/AutomationPracticeAddProductToCart.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -16,7 +17,6 @@ import java.time.Duration;
 public class AutomationPracticeAddProductToCart {
   WebDriver driver;
   WebDriverWait wait;
-  public WebElement actualMessage;
 
 
   @BeforeEach
@@ -37,15 +37,56 @@ public class AutomationPracticeAddProductToCart {
     driver.quit();
   }
 
-@Test
-  public void addProductToCart(){
+  public void add1ProductToCartFromCategoryPage(){
     driver.findElement(By.cssSelector("[id ='homefeatured'] h5 a[title = 'Printed Chiffon Dress']")).click();
     driver.findElement(By.cssSelector("[id= 'add_to_cart'] button")).click();
-
-
-  actualMessage = driver.findElement(By.xpath(".//span[contains(text(),'There is 1 item in your cart.')]"));
-
-  Assertions.assertTrue((actualMessage).isDisplayed(), "Product not in the cart");
-
+    driver.findElement(By.cssSelector("[title='Proceed to checkout']")).click();
   }
+
+  public void add2ProductsToCartFromCategoryPage(){
+    driver.findElement(By.cssSelector("[id ='homefeatured'] h5 a[title = 'Printed Chiffon Dress']")).click();
+    driver.findElement(By.cssSelector("[class= 'icon-plus']")).click();
+    driver.findElement(By.cssSelector("[id= 'add_to_cart'] button")).click();
+    driver.findElement(By.cssSelector("[title='Proceed to checkout']")).click();
+  }
+
+  public void addProductToCartFromHomepage() {
+    Actions act = new Actions(driver);
+    WebElement ele = driver.findElement(By.cssSelector("[id ='homefeatured'] h5 a[title = 'Printed Chiffon Dress']"));
+    WebElement we = driver.findElement(By.cssSelector("[class= 'ajax_block_product col-xs-12 col-sm-4 col-md-3 last-line first-item-of-tablet-line first-item-of-mobile-line last-mobile-line'] [title= 'Add to cart']"));
+    act.moveToElement(ele).moveToElement(we).click().build().perform();
+    driver.findElement(By.cssSelector("[title='Proceed to checkout']")).click();
+  }
+
+  public void checkCart() {
+    driver.findElement(By.cssSelector("div[class= 'shopping_cart'] b")).click();
+  }
+
+  @Test
+  public void addingProductFromCategoryPage(){
+    add1ProductToCartFromCategoryPage();
+    checkCart();
+
+    WebElement actualProduct = driver.findElement(By.cssSelector("td[class= 'cart_description'] p a"));
+    Assertions.assertTrue((actualProduct).isDisplayed(), "No product in the cart");
+  }
+
+  @Test
+  public void adding2ProductsFromCategoryPage(){
+    add2ProductsToCartFromCategoryPage();
+    checkCart();
+
+    String actualDoubleProduct = driver.findElement(By.cssSelector("[name = 'quantity_7_34_0_0_hidden']")).getAttribute("value");
+    Assertions.assertEquals("2", actualDoubleProduct, "Incorrect quantity");
+  }
+
+  @Test
+  public void addingProductFromHomepage() {
+    addProductToCartFromHomepage();
+    checkCart();
+
+    WebElement actualProduct = driver.findElement(By.cssSelector("td[class= 'cart_description'] p a"));
+    Assertions.assertTrue((actualProduct).isDisplayed(), "No product in the cart");
+  }
+
 }
